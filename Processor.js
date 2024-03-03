90ca579b58552e85c41349d03e02256995aa4d32
Gabriel Mendes <contatoeugm@gmail.com>
2024-03-03 20:09:57 -0300

    Feat: added processed data.

---
+++ Processor.js
@@ -0,0 +1,14 @@
+class Processor {
+    static Process(data) {
+        var Dt = data.split("\r\n");
+        var rows = [];
+        
+        Dt.forEach(row => {
+            var arr = row.split(",");
+            rows.push(arr);
+        })
+        console.log(rows);
+    }
+}
+
+module.exports = Processor;
--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-var Reader = require("./Reader")
+const Processor = require("./Processor");
+var Reader = require("./Reader");
 
 var leitor = new Reader();
 
-leitor.Read("./users.csv")
+async function main(){
+    var dados = await leitor.Read("./users.csv");
+    // console.log(dados);
+
+    var DtProcess = Processor.Process(dados);
+    return DtProcess
+}
+
+main();
