0f697e23e70ed811e3951def4b239a35bb1913e6
Gabriel Mendes <contatoeugm@gmail.com>
2024-03-03 23:59:55 -0300

    feat: added Writer

--- app.js
+++ app.js
@@ -2,19 +2,26 @@ const Reader = require("./Reader");
 const Processor = require("./Processor");
 const Table = require("./Table");
 const HtmlParser = require("./HtmlParser");
+const Writer = require("./Writer")
 
 var leitor = new Reader();
+var escritor = new Writer();
 
 async function main(){
     var dados = await leitor.Read("./users.csv");
 
     var DtProcess = Processor.Process(dados);
-    
-    var usuarios = new Table(DtProcess);
-    console.log(usuarios.RowCount);
+    if(Array.isArray(DtProcess)){
+        var usuarios = new Table(DtProcess);
+        console.log(usuarios.RowCount);
+    } else {
+        throw new Error('Not work')
+    }
 
-    const html = await HtmlParser.Parse(usuarios);
+    const html = await HtmlParser.Parse(usuarios); 
     console.log(html);
+
+    escritor.Write(Date.now() + ".html", html);
 }
 
 main();
